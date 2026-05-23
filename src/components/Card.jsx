f400d0bf62eb8660f6a915b9f1210a9e9aa0d7c2
koushik2411 <koushikrk1324@gmail.com>
2026-05-23 16:16:49 +0530

    Modified cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card({ item }) {
   return (
     <div>
       <Link to={item.route}>
-        <div className=" p-3 w-40 md:w-60 lg:w-80 flex flex-col items-center justify-center gap-2 rounded-xl bg-linear-to-b from-slate-200 to-slate-300/60 dark:bg-linear-to-b dark:from-slate-700/90 dark:to-slate-800 text-slate-800 dark:text-slate-100">
+        <div className=" p-3 w-42 md:w-60 lg:w-80 flex flex-col items-center justify-center gap-2 rounded-xl bg-linear-to-b from-slate-200 to-slate-300/60 dark:bg-linear-to-b dark:from-slate-700/90 dark:to-slate-800 text-slate-800 dark:text-slate-100">
           <img src={item.icon} className=" rounded-lg" />
 
           <h1 className=" font-semibold">{item.title}</h1>
